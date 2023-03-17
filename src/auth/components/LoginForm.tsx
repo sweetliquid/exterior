eaad1eb6d5b463bb0a00eb8c481b2cdc527d3aba
sweetliquid <master@sweetliquid.me>
2023-03-17 13:04:10 +0800

    fix: Invalid <Link> with <a> child

--- src/auth/components/LoginForm.tsx
+++ src/auth/components/LoginForm.tsx
@@ -1,11 +1,11 @@
-import { AuthenticationError, PromiseReturnType } from "blitz"
-import Link from "next/link"
-import { LabeledTextField } from "src/core/components/LabeledTextField"
-import { Form, FORM_ERROR } from "src/core/components/Form"
-import login from "src/auth/mutations/login"
-import { Login } from "src/auth/validations"
-import { useMutation } from "@blitzjs/rpc"
-import { Routes } from "@blitzjs/next"
+import { AuthenticationError, PromiseReturnType } from 'blitz'
+import Link from 'next/link'
+import { LabeledTextField } from 'src/core/components/LabeledTextField'
+import { Form, FORM_ERROR } from 'src/core/components/Form'
+import login from 'src/auth/mutations/login'
+import { Login } from 'src/auth/validations'
+import { useMutation } from '@blitzjs/rpc'
+import { Routes } from '@blitzjs/next'
 
 type LoginFormProps = {
   onSuccess?: (user: PromiseReturnType<typeof login>) => void
@@ -20,18 +20,17 @@ export const LoginForm = (props: LoginFormProps) => {
       <Form
         submitText="Login"
         schema={Login}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
+        initialValues={{ email: '', password: '' }}
+        onSubmit={async values => {
           try {
             const user = await loginMutation(values)
             props.onSuccess?.(user)
           } catch (error: any) {
             if (error instanceof AuthenticationError) {
-              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
+              return { [FORM_ERROR]: 'Sorry, those credentials are invalid' }
             } else {
               return {
-                [FORM_ERROR]:
-                  "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+                [FORM_ERROR]: 'Sorry, we had an unexpected error. Please try again. - ' + error.toString(),
               }
             }
           }
@@ -40,17 +39,12 @@ export const LoginForm = (props: LoginFormProps) => {
         <LabeledTextField name="email" label="Email" placeholder="Email" />
         <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
         <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
-          </Link>
+          <Link href={Routes.ForgotPasswordPage()}>Forgot your password?</Link>
         </div>
       </Form>
 
-      <div style={{ marginTop: "1rem" }}>
-        Or{" "}
-        <Link href={Routes.SignupPage()}>
-          <a>Sign Up</a>
-        </Link>
+      <div style={{ marginTop: '1rem' }}>
+        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
       </div>
     </div>
   )
